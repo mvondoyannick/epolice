91f2d5f65faca4f284450ccc37f9cdd3f1566826
MVONDO Yannick <yaf.mvondo@agis-as.com>
2018-10-11 20:42:54 +0200

    validate strucure

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -1,3 +1,15 @@
 class Structure < ApplicationRecord
   belongs_to :region
+
+  #gestion des documents de la structure
+  has_one_attached :document
+  has_one_attached :logo
+
+  #active validation
+  validates :name, presence: {message: "Merci de fournir de nom de la structure"}, uniqueness: {message: "%{value} existe deja."}, length: {minimum: 4, maximum: 50, message: "le nombre de caractere nest pas correcte"}
+  validates :raison, presence: {message: "La raison est obligatoire"}
+  validates :adresse, presence: {message: "Merci de fournir une adresse"}
+  validates :contactname, :contactphone, :contactemail, presence: {message: "Merci de fournir cette information"}
+  validates :contactphone, presence: {message: "Numéro de téléphone obligatoire"}, length: {is: 9, message: "Doit avoir 9 chiffres"}, numericality: {only_integer: true, message: "Ce numéro n'est pas valide, uniquement des chiffres"}
+
 end
