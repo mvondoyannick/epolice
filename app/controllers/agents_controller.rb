2bef2b85a210c01c5791b1b7cd5fcf418c1e1a27
MVONDO Yannick <yaf.mvondo@agis-as.com>
2018-11-03 15:29:10 +0100

    update agent new query

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -7,7 +7,7 @@ class AgentsController < ApplicationController
   def index
     respond_to do |format|
       format.html do
-        @agents = Agent.order(name: :asc).where(statusdeleted: false)
+        @agents = Agent.order(name: :asc)
       end
       format.xls
     end
