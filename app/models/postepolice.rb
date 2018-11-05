af619f300d3b6579827d99edb31a372369116808
MVONDO Yannick <yaf.mvondo@agis-as.com>
2018-11-05 16:22:56 +0100

    update model poste police

--- app/models/postepolice.rb
+++ app/models/postepolice.rb
@@ -1,13 +1,7 @@
 class Postepolice < ApplicationRecord
-  belongs_to :agent
   belongs_to :commissariat
   belongs_to :region
-  #belongs_to :grade
 
   validates :name, presence: {message: 'Merci d indiquer le nom du poste'}, uniqueness: {message: '%{value} existe déja'}
   validates :location, presence: {message: 'Merci de fournir la localisation'}, uniqueness: {message: '%{value} existe deja'}
-  #validates :email, :phone, presence: {message: 'Informations manquante'}, uniqueness: true
-
-  validates_presence_of :name, presence: true, presence: {message: 'Merci de fournir le nom du poste.'}
-  validates_presence_of :location, presence: true, presence: {message: 'Merci de decrire la localisation'}
 end
