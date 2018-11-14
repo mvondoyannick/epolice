7830ad92a132bf0597651d36ff99e13b0b445399
MVONDO Yannick <yaf.mvondo@agis-as.com>
2018-11-14 12:49:53 +0100

    update API logic

--- app/controllers/api/convocations_controller.rb
+++ app/controllers/api/convocations_controller.rb
@@ -40,7 +40,7 @@ class Api::ConvocationsController < ApplicationController
             unite_id: data.groupement_id,
             region: Groupement.find(data.groupement_id).name.split[1],
             #unite: data.groupement.split[0],
-            region: data.groupement.split[1],
+            #region: data.groupement.split[1],
             #region2: data.region.name,
             ip_geolocation: JSON.parse(Net::HTTP.get(URI('https://ipapi.co/'+request.remote_ip+'/json/'))),
             apikey: SecureRandom.hex(10),
